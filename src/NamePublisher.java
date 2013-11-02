0d53539d44340b0b6dccf420b8abcfdd5c6e9115
timmik94 <timmik94@mail.ru>
2013-11-02 15:13:16 +0400

    java class

---
+++ src/NamePublisher.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class NamePublisher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> nameList=new ArrayList<String>();
+		nameList.add("Azizov Timur");
+		
+
+	}
+
+}
